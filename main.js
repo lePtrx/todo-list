7f0361641dfd9e4191cca3daf9ed30a96d059a29
lePtrx <leptrx@gmail.com>
2019-11-02 16:39:25 +0800

    Added 'Add Todos into List' function

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
-const deleteButton = document.querySelector(".delete-btn");
-const listItem = document.querySelector(".list-item");
+// const deleteButton = document.querySelector(".delete-btn");
+// const listItem = document.querySelector(".list-item");
 
-deleteList = () => {
-  listItem.remove();
-};
+// deleteList = () => {
+//   listItem.remove();
+// };
 
-deleteButton.addEventListener("click", deleteList);
+// deleteButton.addEventListener("click", deleteList);
+
+// To add new todos into list
+
+var addTodos = document.querySelector(".add-list-form");
+var ul = document.querySelector(".list-all");
+
+addTodos.addEventListener("keypress", function(keyPressed) {
+  if (keyPressed.which === 13) {
+    var li = document.createElement("li");
+    li.setAttribute("class", "list-item");
+    var p = document.createElement("p");
+    p.setAttribute("class", "item");
+    var listItem = document.createElement("span");
+    var deleteIcon = document.createElement("i");
+
+    var newTodo = this.value;
+    this.value = " ";
+
+    deleteIcon.classList.add("far", "fa-trash-alt", "delete-btn");
+    listItem.append(deleteIcon);
+    ul.appendChild(li)
+      .appendChild(p)
+      .append(newTodo, listItem);
+
+    deleteTodo();
+  }
+});
